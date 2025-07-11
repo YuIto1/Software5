f88bec58d6095a65b958958dfd03cde78b2b944b
1W232302-4algo <the.for.at.reality2@gmail.com>
2025-07-11 11:28:42 +0900

    Update RoomReservationScreen.java

--- HRS2/RoomReservationScreen.java
+++ HRS2/RoomReservationScreen.java
@@ -6,6 +6,23 @@ public class RoomReservationScreen {
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("管理者ですか（y / [n]）: ");
+        String isAdminString = scanner.nextLine();
+        boolean isAdmin = isAdminString.equals("y");
+
+        if (isAdmin) {
+            System.out.print("操作を選択してください。（0:部屋登録, 1: 予約情報）");
+            int op = scanner.nextInt();
+            if (op == 0) {
+                control.addRoom();
+            }else if(op == 1){
+                control.displayAllReservations();
+            }
+            
+            scanner.close();
+            return;
+        }
+
         System.out.print("部屋タイプを選んでください（standard / suite）: ");
         String roomTypeStr = scanner.nextLine();
 
